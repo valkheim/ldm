b18eddb84d93db0a6e7a31cc39b1c69c78d27bf8
Charles Paulet <charles.paulet@epitech.eu>
2018-02-06 14:25:30 +0100

    Handle switch mode

--- srcs/events.c
+++ srcs/events.c
@@ -5,46 +5,85 @@
 #include "main.h"
 #include "events.h"
 
-static void print_modifiers (uint32_t mask)
+static void print_modifiers(xcb_keycode_t detail, uint32_t state)
 {
   const char **mod, *mods[] = {
     "Shift", "Lock", "Ctrl", "Alt",
     "Mod2", "Mod3", "Mod4", "Mod5",
     "Button1", "Button2", "Button3", "Button4", "Button5"
   };
-  printf ("Modifier mask: ");
-  for (mod = mods ; mask; mask >>= 1, mod++)
-    if (mask & 1)
+  printf("xcb: keypress: code: %d mod: %d (", detail, state);
+  for (mod = mods ; state; state >>= 1, mod++)
+    if (state & 1)
       printf("%s", *mod);
-  putchar ('\n');
+  puts(")");
 }
 
-static xcb_keysym_t xcb_get_keysym(xcb_keycode_t detail) {
-    xcb_key_symbols_t *keysyms;
-    xcb_keysym_t keysym;
+static xcb_keysym_t xcb_get_keysym(xcb_keycode_t detail, uint16_t state)
+{
+  /*
+   * adapt keysym using detail (modifier)
+   * https://github.com/Cloudef/monsterwm-xcb/blob/master/monsterwm.c
+   * https://lists.freedesktop.org/archives/xcb/2010-February/005562.html
+   * https://github.com/ehntoo/unagi/blob/master/src/key.c
+   * /usr/include/xcb/xproto.h:500
+   */
+  xcb_key_symbols_t *keysyms;
+  xcb_keysym_t k0, k1;
 
-    if (!(keysyms = xcb_key_symbols_alloc(c)))
-      return 0;
+  if (!(keysyms = xcb_key_symbols_alloc(c)))
+    return 0;
 
-    keysym = xcb_key_symbols_get_keysym(keysyms, detail, 0);
+  if (state & XCB_MOD_MASK_5) {
+    k0 = xcb_key_symbols_get_keysym(keysyms, detail, 4);
+    k1 = xcb_key_symbols_get_keysym(keysyms, detail, 5);
+  } else {
+    k0 = xcb_key_symbols_get_keysym(keysyms, detail, 0);
+    k1 = xcb_key_symbols_get_keysym(keysyms, detail, 1);
+  }
 
-    /*
-     * adapt keysym using detail (modifier)
-     * https://github.com/Cloudef/monsterwm-xcb/blob/master/monsterwm.c
-     * https://lists.freedesktop.org/archives/xcb/2010-February/005562.html
-     */
+  xcb_key_symbols_free(keysyms);
 
-    xcb_key_symbols_free(keysyms);
-    return keysym;
+  if (xcb_is_keypad_key(k1))
+  {
+    /* The Shift modifier  is on, or if the Lock  modifier is on and
+     * is interpreted as ShiftLock, use the first KeySym */
+    if ((state & XCB_MOD_MASK_SHIFT) || (state & XCB_MOD_MASK_LOCK)) {
+      return k0;
+    } else {
+      return k1;
+    }
+    /* The Shift and Lock modifers are both off, use the first KeySym */
+  } else if (!(state & XCB_MOD_MASK_SHIFT) && !(state & XCB_MOD_MASK_LOCK)) {
+    return k0;
+    /* The Shift  modifier is  off and  the Lock modifier  is on  and is
+     * interpreted as CapsLock */
+  } else if (!(state & XCB_MOD_MASK_SHIFT) && (state & XCB_MOD_MASK_LOCK)) {
+    /* The  first Keysym  is  used  but if  that  KeySym is  lowercase
+     * alphabetic,  then the  corresponding uppercase  KeySym  is used
+     * instead */
+    return k1;
+    /* The Shift modifier is on, and the Lock modifier is on and is
+     * interpreted as CapsLock */
+  } else if((state & XCB_MOD_MASK_SHIFT) && (state & XCB_MOD_MASK_LOCK)) {
+    /* The  second Keysym  is used  but  if that  KeySym is  lowercase
+     * alphabetic,  then the  corresponding uppercase  KeySym  is used
+     * instead */
+    return k1;
+    /* The  Shift modifer  is on,  or  the Lock  modifier is  on and  is
+     * interpreted as ShiftLock, or both */
+  } else if((state & XCB_MOD_MASK_SHIFT) || (state & XCB_MOD_MASK_LOCK)) {
+    return k1;
+  }
+  return XCB_NO_SYMBOL;
 }
 
 static void key_press_management(xcb_key_press_event_t *ev)
 {
   xcb_keysym_t keysym;
 
-  print_modifiers(ev->state);
-  printf("xcb: keypress: code: %d mod: %d\n", ev->detail, ev->state);
-  keysym = xcb_get_keysym(ev->detail);
+  print_modifiers(ev->detail, ev->state);
+  keysym = xcb_get_keysym(ev->detail, ev->state);
   printf("xcb: keysym: %c\n", keysym);
 }
 
@@ -95,7 +134,7 @@ void dm_event_loop()
         //  break;
       default:
         event_management(e);
-        printf("The events = %s\n",xcb_event_get_label(e->response_type));
+        printf("event = %s\n",xcb_event_get_label(e->response_type));
     }
     free(e);
   }
