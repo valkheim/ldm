71588f095707fbbbdda0ced4806d337015996db4
Charles Paulet <charles.paulet@epitech.eu>
2018-02-19 00:10:20 +0100

    Fix copyright date

--- srcs/draw.c
+++ srcs/draw.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of the ldm distribution (https://github.com/valkheim/ldm)
- * Copyright (c) BORDER_WIDTH18 Charles Paulet.
+ * Copyright (c) 2018 Charles Paulet.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
